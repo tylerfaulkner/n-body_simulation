265266bb4a1ab82546e106f4cd51c0e9bc0c976f
Tyler Faulkner <tylersfaulkner@gmail.com>
2022-10-24 18:31:43 -0500

    Implemented Randomized Body Positions on CPU

--- src/nbody_init.cuh
+++ src/nbody_init.cuh
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curand.h> //cuda random library
 
-__global__ void initialzeBodies(void *d_X) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-
-    float4 *globalX = (float *)d_X;
-
-    /*
-    TODO
-    */
-    //float xPos = 
+void initializeBodies(float4 *h_X, int n) {
+    for(int i=0; i<n; i++){
+        float xPos = (float)rand();
+        float yPos = (float)rand();
+        float zPos = (float)rand();
+        float mass = (float)rand();
+        h_X[i] = {xPos, yPos, zPos, mass};
+    }
 }
