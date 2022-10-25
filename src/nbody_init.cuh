a9c878bd676808f0e846f8b482fdc2f9e668833f
Tyler Faulkner <tylersfaulkner@gmail.com>
2022-10-25 17:35:45 -0400

    Added Example Results Folder

--- src/nbody_init.cuh
+++ src/nbody_init.cuh
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_DIM 50
+#define MAX_DIM 200
 #define MOD_DIM MAX_DIM*2
+#define MAX_MASS 100
 
 void initializeBodies(double4 *h_X, int n) {
     for(int i=0; i<n; i++){
         double xPos = (double)((rand() % MOD_DIM)-MAX_DIM);
         double yPos = (double)((rand() % MOD_DIM)-MAX_DIM);
         double zPos = (double)((rand() % MOD_DIM)-MAX_DIM);
-        double mass = (double)((rand() % MOD_DIM)-MAX_DIM);
+        double mass = (double)((rand() % MAX_MASS));
         h_X[i] = {xPos, yPos, zPos, mass};
     }
 }
