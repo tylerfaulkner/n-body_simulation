ebe00c43950fa5ee142aad2d68b5b3a1dc9bbdc2
tylerfaulkner <tylersfaulkner@gmail.com>
2022-11-10 11:25:00 -0600

    Made CPU use same function as gpu

--- src/nbody_cpu.h
+++ src/nbody_cpu.h
@@ -4,27 +4,6 @@
 
 #define EPS2 0.01 //EPSILON SQUARED dampening factor in textbook, used to prevent values to approach infinity
 
-float3 bodyBodyInteraction_cpu(float4 bi, float4 bj, float3 ai)
-{
-    float3 r; //vector between bodies
-    // r_ij [3 FLOPS]
-    r.x = bj.x - bi.x;
-    r.y = bj.y - bi.y;
-    r.z = bj.z - bi.z;
-    // distSqr = dot(r_ij, r_ij) + EPS^2 [6 FLOPS]
-    float distSqr = r.x * r.x + r.y * r.y + r.z * r.z + EPS2;
-    // invDistCube =1/distSqr^(3/2) [4 FLOPS (2 mul, 1 sqrt, 1 inv)]
-    float distSixth = distSqr * distSqr * distSqr;
-    float invDistCube = 1.0f/sqrt(distSixth);
-    // s = m_j * invDistCube [1 FLOP]
-    float s = bj.w * invDistCube;
-    // a_i = a_i + s * r_ij [6 FLOPS]
-    ai.x += r.x * s;
-    ai.y += r.y * s;
-    ai.z += r.z * s;
-    return ai;
-}
-
 void calculate_forces(float4 *h_X, float4 *h_A, int bodyCount)
 {
     float4 myPosition;
@@ -33,7 +12,7 @@ void calculate_forces(float4 *h_X, float4 *h_A, int bodyCount)
         float3 acc = {0.0f, 0.0f, 0.0f};
         myPosition = h_X[currentBody];
         for (i = 0; i < bodyCount; i++) {
-            acc = bodyBodyInteraction_cpu(myPosition, h_X[i], acc);
+            acc = bodyBodyInteraction(myPosition, h_X[i], acc);
         }
         float4 acc4 = {acc.x, acc.y, acc.z, 0.0f};
         h_A[currentBody] = acc4;
